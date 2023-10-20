1328b7648c16d3fdb119f0daacf6f606ed5905c0
ARPIT SAGAR <98328210+ARPIT226@users.noreply.github.com>
2023-10-20 11:15:50 +0530

    Oath of the Night's Watch

---
+++ 768A.cpp
@@ -0,0 +1,31 @@
+/* Question link: https://codeforces.com/problemset/problem/768/A
+Tag: constructive algorithms, sortings, 900*
+Codeforces ID: arpit_26 */
+
+
+#include <bits/stdc++.h>
+using namespace std;
+void solve(){
+    int n,cnt=0; cin>>n;
+    vector <int> v;
+    for(int i=0;i<n;i++){
+        int x; cin>>x;
+        v.push_back(x);
+    }
+    sort(v.begin(),v.end());
+    for(int i=0;i<n;i++){
+        if(v[i]==v[0] || v[i]==v[n-1]) cnt++;
+    }
+    cout<<n-cnt<<endl;
+   
+}
+ 
+int main()
+{
+    int t=1;
+    // cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+}
