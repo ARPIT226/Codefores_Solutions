e203205647ebcd0ad5f10ec9302b305cce397a6d
ARPIT SAGAR <98328210+ARPIT226@users.noreply.github.com>
2023-10-26 23:22:58 +0530

    Mystic Permutation

---
+++ 1689B.cpp
@@ -0,0 +1,26 @@
+/* Question link: https://codeforces.com/problemset/problem/1689/B
+Tag: data structures, greedy, 900*
+Codeforces ID: arpit_26 */
+
+
+
+# include <bits/stdc++.h>
+using namespace std;
+ 
+void solve(){
+    long long a,b;  
+    cin>>a>>b;
+    if(a-b==1){
+        cout<<"NO"<<endl;
+        return;
+    }
+    else cout<<"YES"<<endl;
+}
+ 
+int main(){
+    
+    int t; cin>>t;
+    while(t--){
+        solve();
+    }
+}
